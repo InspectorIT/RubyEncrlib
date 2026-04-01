6a74ee86b87c345a53dfa76179ea6a1fa59028a8
InspectorIT <sshipunovv008@gmail.com>
2026-04-01 20:36:36 +0300

    add more tests for rail_fence cipher and decipher

--- test/test_rail_fence_decipher.rb
+++ test/test_rail_fence_decipher.rb
@@ -14,4 +14,47 @@ class RailFenceDecipherTest < Minitest::Test
 
     assert_equal source, rail_fence_decipher(encrypted, 4)
   end
+
+  def test_rail_fence_decrypts_with_two_rails
+    assert_equal 'ATTACKATDAWN', rail_fence_decipher('ATCADWTAKTAN', 2)
+  end
+
+  def test_rail_fence_decrypts_with_three_rails
+    assert_equal 'HELLOWORLD', rail_fence_decipher('HOLELWRDLO', 3)
+  end
+
+  def test_rail_fence_decrypts_digits_and_symbols
+    assert_equal '12345-!', rail_fence_decipher('135!24-', 2)
+  end
+
+  def test_rail_fence_decrypts_cyrillic_and_spaces
+    encrypted = 'П, рт y2!иеRb06вu2'
+
+    assert_equal 'Привет, Ruby 2026!', rail_fence_decipher(encrypted, 4)
+  end
+
+  def test_rail_fence_round_trip_with_mixed_text
+    source = 'Secret + Секрет: 42!'
+    encrypted = rail_fence_cipher(source, 4)
+
+    assert_equal source, rail_fence_decipher(encrypted, 4)
+  end
+
+  def test_rail_fence_keeps_input_for_single_rail
+    assert_equal 'abc', rail_fence_decipher('abc', 1)
+  end
+
+  def test_rail_fence_keeps_input_when_key_is_zero_or_negative
+    assert_equal 'abc', rail_fence_decipher('abc', 0)
+    assert_equal 'abc', rail_fence_decipher('abc', -3)
+  end
+
+  def test_rail_fence_keeps_input_when_text_is_too_short
+    assert_equal 'x', rail_fence_decipher('x', 5)
+    assert_equal '', rail_fence_decipher('', 4)
+  end
+
+  def test_rail_fence_keeps_input_when_key_is_larger_than_text
+    assert_equal 'abc', rail_fence_decipher('abc', 5)
+  end
 end

26cb4973f2e60b42f39ee5a4fca94579f9f7a10b
InspectorIT <sshipunovv008@gmail.com>
2026-04-09 08:33:03 +0300

    add extra methods

--- lib/rail_fence.rb
+++ lib/rail_fence.rb
@@ -1,5 +1,31 @@
 # frozen_string_literal: true
 
+# @param [Integer] length
+# @param [Integer] key
+def build_pattern(length, key)
+  pattern = []
+  rail = 0
+  direction = 1
+
+  length.times do
+    pattern << rail
+
+    direction = 1 if rail.zero?
+    direction = -1 if rail == key - 1
+    rail += direction
+  end
+
+  pattern
+end
+
+# @param [Array] pattern
+# @param [Integer] key
+def rail_lengths_from_pattern(pattern, key)
+  lengths = Array.new(key, 0)
+  pattern.each { |i| lengths[i] += 1 }
+  lengths
+end
+
 # string encryption with Rail Fence cipher|Шифрование строки методом Железнодорожной изгороди
 # @param str [String] input string|исходная строка
 # @param key [Integer] rails count|количество рельс
@@ -7,16 +33,11 @@
 def rail_fence_cipher(str, key)
   return str if key <= 1 || str.size <= 1
 
+  pattern = build_pattern(str.size, key)
   rails = Array.new(key) { '' }
-  rail = 0
-  direction = 1
 
-  str.each_char do |ch|
-    rails[rail] += ch
-
-    direction = 1 if rail.zero?
-    direction = -1 if rail == key - 1
-    rail += direction
+  str.each_char.with_index do |ch, i|
+    rails[pattern[i]] += ch
   end
 
   rails.join
@@ -29,35 +50,24 @@ end
 def rail_fence_decipher(str, key)
   return str if key <= 1 || str.size <= 1
 
-  pattern = []
-  rail = 0
-  direction = 1
-
-  str.size.times do
-    pattern << rail
-
-    direction = 1 if rail.zero?
-    direction = -1 if rail == key - 1
-    rail += direction
-  end
-
-  rail_lengths = Array.new(key, 0)
-  pattern.each { |idx| rail_lengths[idx] += 1 }
+  pattern = build_pattern(str.size, key)
+  rail_lengths = rail_lengths_from_pattern(pattern, key)
 
   rails = []
   cursor = 0
+
   rail_lengths.each do |len|
     rails << str[cursor, len].chars
     cursor += len
   end
 
   rail_positions = Array.new(key, 0)
-  decstr = ''
+  result = ''
 
   pattern.each do |idx|
-    decstr += rails[idx][rail_positions[idx]]
+    result += rails[idx][rail_positions[idx]]
     rail_positions[idx] += 1
   end
 
-  decstr
+  result
 end
